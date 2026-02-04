fe202304d3039ef48969cc074b8b2b8ffedbcc60
Lasse Diercks <github@lassediercks.de>
2026-02-04 22:57:29 +0100

    chore: bump to 0.0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plato.MixProject do
   def project do
     [
       app: :plato,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
