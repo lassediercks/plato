a9dc8d2dfbbb296a7e32f5afecd9585f10432bf6
Lasse Diercks <github@lassediercks.de>
2026-02-05 23:27:17 +0100

    chore(main): release 0.0.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plato.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.0.7",
+      version: "0.0.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
