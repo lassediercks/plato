b049c8079e24c2977e21c93402ef9a55aae00145
Lasse Diercks <github@lassediercks.de>
2026-02-07 01:10:56 +0100

    chore(main): release 0.0.19

--- apps/plato/mix.exs
+++ apps/plato/mix.exs
@@ -5,7 +5,7 @@ defmodule Plato.MixProject do
     [
       app: :plato,
       # x-release-please-start-version
-      version: "0.0.18",
+      version: "0.0.19",
       # x-release-please-end
       build_path: "../../_build",
       deps_path: "../../deps",
@@ -77,7 +77,7 @@ defmodule Plato.MixProject do
       main: "readme",
       extras: ["README.md", "CHANGELOG.md"],
       # x-release-please-start-version
-      source_ref: "v0.0.18",
+      source_ref: "v0.0.19",
       # x-release-please-end
       source_url: "https://github.com/lassediercks/plato"
     ]
